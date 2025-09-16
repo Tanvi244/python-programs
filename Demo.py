697539c99c0e3b5ee383fec8fe2eb7cd33cc7ded
Tanvi244 <134038333+Tanvi244@users.noreply.github.com>
2025-09-16 14:31:57 -0400

    Add files via upload

--- Demo.py
+++ Demo.py
@@ -1,1 +1,10 @@
-print("first program of python...")
+print("First Program of Python...")
+
+# To compile the python Code
+# python Demo.py
+
+# To create .pyc file
+# python -m compileall Demo.py
+# It creates .pyc file in __pycache__ folder
+# To execute the file
+# python Demo.cpython-310.pyc
---
+++ Function1.py
@@ -0,0 +1,14 @@
+
+# Defination of Function
+def Fun():
+    print("Inside Fun")#function body
+    print("Inside Python")
+
+
+# Function Call
+Fun()
+
+def Gun():
+    pass
+
+Gun()

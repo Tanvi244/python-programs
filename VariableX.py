ba0b3f184f8a5925f682841fe21dc4a21bcaa2df
Tanvi244 <134038333+Tanvi244@users.noreply.github.com>
2026-01-30 13:04:52 -0500

    Add files via upload

--- VariableX.py
+++ VariableX.py
@@ -1,13 +1,11 @@
-X = 11  #integer
-
+X = 11
 print(type(X))
 
 X = 67.90
 print(type(X))
 
-X = 'python'
+X = 'Python'
 print(type(X))
-
 
 X = False
 print(type(X))
@@ -15,15 +13,12 @@ print(type(X))
 X = 89j
 print(type(X))
 
-
 A = B = C = 11
 print(A)
 print(B)
 print(C)
 
-P, Q, R = 10, 20, 30
+P, Q, R = 10,20,30
 print(P)
 print(Q)
 print(R)
-
-
